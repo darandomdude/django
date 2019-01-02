5c3aedc6d229e18c6726f9f10d9847e83192b4ed
Roman <mr.rostar@gmail.com>
2019-01-02 15:56:30 +0500

    Added DB modesl

---
+++ shop/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+# Класс Категория товара
+class Category(models.Model):
+    name = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, db_index=True, unique=True)
+
+# Класс Товар, связь --> Категория, один ко многим
+class Product(models.Model):
+    category = models.ForeignKey(Category, related_name='products')
+    name = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, db_index=True, unique=True)
+    image = models.ImageField(upload_to='products/%y/%m/%d', blank=True)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+class Meta(models.Model):
+    ordering = ('-created')
+    index_together = (('id','slug'),)
+
