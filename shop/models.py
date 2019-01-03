9ba9beba3f0e18551ce1426a0e7907a442c12502
Roman <mr.rostar@gmail.com>
2019-01-03 18:06:45 +0500

    First commit

---
+++ shop/models.py
@@ -0,0 +1,44 @@
+from django.db import models
+# from django.urls import reverse
+
+
+# Класс Категория товара
+class Category(models.Model):
+    name = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, db_index=True, unique=True)
+
+#    class Meta:
+#        ordering = 'name'
+#        verbose_name = 'category'
+#        verbose_name_plural = 'categories'
+
+#        def __str__(self):
+#            return self.name
+
+#        def get_absolute_url(self):
+#            return reverse('shop:product_list_by_category', args=[self.slug])
+
+
+# Класс Товар, связь --> Категория, один ко многим
+class Product(models.Model):
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, db_index=True, unique=True)
+    image = models.ImageField(upload_to='products/%y/%m/%d', blank=True)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+
+#    class Meta(models.Model):
+#        ordering = '-created'
+#        index_together = (('id', 'slug'),)
+
+#        def __str__(self):
+#            return self.name
+
+#        def get_absolute_url(self):
+#            return reverse('shop:product_list_by_category', args=[self.id, self.slug])
